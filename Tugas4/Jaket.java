42ad67a8fd910b8647c36b4598a1f04feae80fdf
sriwahyuni-aj <ajsriwahyuni@gmail.com>
2017-03-07 23:17:32 +0700

    Add files via upload

---
+++ Tugas4/Jaket.java
@@ -0,0 +1,12 @@
+public class Jaket{
+  protected String jaket, ;
+  protected Integer harga;
+  
+
+  public void Informasi_jaket() {
+	
+    System.out.print("NAMA    : " + jaket + " \n");
+    System.out.print("Harga : " + harga + "\n");
+    System.out.print("\n");
+  }
+}
---
+++ Tugas4/Loundry.java
@@ -0,0 +1,20 @@
+public class Loundry {
+	public static void main (String[] args) {
+	System.out.print("-- INFORMASI JAKET --\n\n");
+	jaket = new Jaket();
+ 
+	
+	
+    jaket.nama = "JAKET";
+    jaket.harga = 15000;
+    jaket.Informasi_jaket();
+	
+	
+	System.out.print("-- INFORMASI SELIMUT --\n\n");
+	selimut = new SELIMUT();
+	
+    selimut.nama = "SELIMUT";
+    selimut.harga = 20000;
+    selimut.informasi_selimut();
+	}
+}
